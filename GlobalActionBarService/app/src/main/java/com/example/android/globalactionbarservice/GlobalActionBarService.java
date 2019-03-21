ab10ab76fb4aa0d88243d5d144cb580a73337a4e
lg0491986 <lg0491986@qq.com>
2019-03-21 20:09:38 +0800

    basic auto install version

--- GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
+++ GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
@@ -19,6 +19,8 @@ import android.accessibilityservice.GestureDescription;
 import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.media.AudioManager;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,17 +29,139 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.List;
 
 public class GlobalActionBarService extends AccessibilityService {
+    private static final String TAG = GlobalActionBarService.class.getSimpleName();
+    private static final int DELAY_PAGE = 320; // 页面切换时间
+    private final Handler mHandler = new Handler();
+
+    public void onServiceConnected(){
+        Log.i("AccessibilityService", "onServiceConnected");
+        Toast.makeText(this, getString(R.string.aby_label) + "开启了", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy: ");
+        Toast.makeText(this, getString(R.string.aby_label) + "停止了，请重新开启", Toast.LENGTH_LONG).show();
+        // 服务停止，重新进入系统设置界面
+        AccessibilityUtil.jumpToSetting(this);
+    }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.i(TAG, "onAccessibilityEvent: " + event);
+        final int eventType = event.getEventType();
+        String eventText = null;
+        switch(eventType) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                eventText = "Focused: ";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                eventText = "Focused: ";
+                break;
+        }
+        Log.i(TAG, "" + eventText);
 
+       Log.i(TAG, "onAccessibilityEvent: " + event.getPackageName());
+
+        if (event == null || !event.getPackageName().toString()
+                .contains("packageinstaller")){//不写完整包名，是因为某些手机(如小米)安装器包名是自定义的
+            Log.i(TAG, "Can't get catch PackageName");
+            return;
+        }
+
+        /*
+        模拟点击->自动安装，只验证了小米5s plus(MIUI 9.8.4.26)、小米Redmi 5A(MIUI 9.2)、华为mate 10
+        其它品牌手机可能还要适配，适配最可恶的就是出现安装广告按钮，误点安装其它垃圾APP（典型就是小米安装后广告推荐按钮，华为安装开始官方安装）
+        */
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow(); //当前窗口根节点
+        if (rootNode == null)
+            return;
+        Log.i(TAG, "rootNode: " + rootNode);
+        if (isNotAD(rootNode))
+            findTxtClick(rootNode, "安装"); //一起执行：安装->下一步->打开,以防意外漏掉节点
+        findTxtClick(rootNode, "继续");
+        findTxtClick(rootNode, "安装");
+        findTxtClick(rootNode, "下一步");
+        findTxtClick(rootNode, "打开");
+        // 回收节点实例来重用
+        rootNode.recycle();
     }
 
+    public void serviceEventHandle(AccessibilityEvent event){
+        if (event == null || !event.getPackageName().toString()
+                .contains("packageinstaller")){//不写完整包名，是因为某些手机(如小米)安装器包名是自定义的
+            Log.i(TAG, "Can't get catch PackageName");
+            return;
+        }
+        /*
+        某些手机安装页事件返回节点有可能为null，无法获取安装按钮
+        例如华为mate10安装页就会出现event.getSource()为null，所以取巧改变当前页面状态，重新获取节点。
+        该方法在华为mate10上生效，但其它手机没有验证...(目前小米手机没有出现这个问题)
+        */
+        Log.i(TAG, "onAccessibilityEvent: " + event);
+        AccessibilityNodeInfo eventNode = event.getSource();
+        if (eventNode == null) {
+            Log.i(TAG, "eventNode: null, 重新获取eventNode...");
+            performGlobalAction(GLOBAL_ACTION_RECENTS); // 打开最近页面
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    performGlobalAction(GLOBAL_ACTION_BACK); // 返回安装页面
+                }
+            }, DELAY_PAGE);
+            return;
+        }
+
+        /*
+        模拟点击->自动安装，只验证了小米5s plus(MIUI 9.8.4.26)、小米Redmi 5A(MIUI 9.2)、华为mate 10
+        其它品牌手机可能还要适配，适配最可恶的就是出现安装广告按钮，误点安装其它垃圾APP（典型就是小米安装后广告推荐按钮，华为安装开始官方安装）
+        */
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow(); //当前窗口根节点
+        if (rootNode == null)
+            return;
+        Log.i(TAG, "rootNode: " + rootNode);
+        if (isNotAD(rootNode))
+            findTxtClick(rootNode, "安装"); //一起执行：安装->下一步->打开,以防意外漏掉节点
+        findTxtClick(rootNode, "继续安装");
+        findTxtClick(rootNode, "下一步");
+        findTxtClick(rootNode, "打开");
+        // 回收节点实例来重用
+        eventNode.recycle();
+        rootNode.recycle();
+    }
+
+    // 查找安装,并模拟点击(findAccessibilityNodeInfosByText判断逻辑是contains而非equals)
+    private void findTxtClick(AccessibilityNodeInfo nodeInfo, String txt) {
+        List<AccessibilityNodeInfo> nodes = nodeInfo.findAccessibilityNodeInfosByText(txt);
+        if (nodes == null || nodes.isEmpty())
+            return;
+        Log.i(TAG, "findTxtClick: " + txt + ", " + nodes.size() + ", " + nodes);
+        for (AccessibilityNodeInfo node : nodes) {
+            if (node.isEnabled() && node.isClickable() && (node.getClassName().equals("android.widget.Button")
+                    || node.getClassName().equals("android.widget.CheckBox") // 兼容华为安装界面的复选框
+            )) {
+                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+    }
+
+    // 排除广告[安装]按钮
+    private boolean isNotAD(AccessibilityNodeInfo rootNode) {
+        return isNotFind(rootNode, "还喜欢") //小米
+                && isNotFind(rootNode, "官方安装"); //华为
+    }
+
+    private boolean isNotFind(AccessibilityNodeInfo rootNode, String txt) {
+        List<AccessibilityNodeInfo> nodes = rootNode.findAccessibilityNodeInfosByText(txt);
+        return nodes == null || nodes.isEmpty();
+    }
     @Override
     public void onInterrupt() {
 
